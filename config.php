62485eeb28521969d9b69026301d7d7c8c92e03b
Muhammad Risky <56730687+riskyfreak@users.noreply.github.com>
2019-11-06 04:29:32 -0800

    Update config.php

--- config.php
+++ config.php
@@ -2,5 +2,5 @@
 
 $email = "risky.coin666@gmail.com";
 $password = "s1lentfr3ak666";
-$PHPSESSID = "ucj6c3kfaho00qa9v8999lpf11";
-$cfduid = "dc908515df16b1db6fbd4e8eddcd37e591573042586";
+$PHPSESSID = "PHPSESSID=ucj6c3kfaho00qa9v8999lpf11";
+$cfduid = "__cfduid=dc908515df16b1db6fbd4e8eddcd37e591573042586";
